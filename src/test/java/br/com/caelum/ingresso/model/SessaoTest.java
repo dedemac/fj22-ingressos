00e32893dde2f6cd70ce279773caf723179526c7
dedemac <dedemac@me.com>
2018-08-18 17:02:01 -0300

    final

---
+++ src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
@@ -0,0 +1,20 @@
+package br.com.caelum.ingresso.model;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class SessaoTest {
+@Test
+public void oPrecoDaSessaoDeveSerIgualASomaDoPrecoDaSalaMaisOPrecoDoFilme(){
+	Sala sala = new Sala("Eldorado", new BigDecimal("22.5"));
+	Filme filme = new Filme("x", Duration.ofMinutes(120), "SCI", new BigDecimal("12.0"));
+	
+	BigDecimal somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
+	Sessao sessao = new Sessao(LocalTime.parse("10:00:00"),filme,sala);
+	Assert.assertEquals( somaDosPrecosDaSalaEFilme,  sessao.getPreco());
+}}
